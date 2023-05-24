50211ebef02c8a0591d034b7c7d53d49ca3779c7
Asgar-Asgarov <asgar.y.asgarov@gmail.com>
2023-05-24 15:29:13 +0400

    feature:some sx and fontweight properties added Typography

--- frontend/src/components/HeroBanner.js
+++ frontend/src/components/HeroBanner.js
@@ -8,9 +8,27 @@ const HeroBanner = () => {
       mt:{lg:'212px',xs:'70px'},
       ml:{sm:'50px'}
     }}
+    position="relative"
+    p="20px"
     >
       <Typography color="#FF2625" fontWeight="600" fontSize="26px">
         Fitness Club
+      </Typography>
+      <Typography fontWeight={700}
+      sx={{
+        fontSize:{
+          lg:'44px',
+          xs:'40px'
+        }
+      }}
+      >
+        Sweat, Smile <br/> and Repeat
+      </Typography>
+      <Typography
+      fontSize='22px'
+      lineHeight="35px"
+      >
+        Check out the most effective exercises
       </Typography>
     </Box>
   );
