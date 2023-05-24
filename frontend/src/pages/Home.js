d1f5b00a157369e7dec6145c17e9a0538f92a8b9
Asgar-Asgarov <asgar.y.asgarov@gmail.com>
2023-05-24 12:32:04 +0400

    feature:HeroBanner,Exercises,SearchExercises components were created

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,5 +1,8 @@
 import React,{useState} from 'react';
 import { Box } from '@mui/material';
+import HeroBanner from '../components/HeroBanner';
+import Exercises from '../components/Exercises';
+import SearchExercises from '../components/SearchExercises';
 
 const Home = () => {
   return (
