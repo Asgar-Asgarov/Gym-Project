3956da8f574f04ac3e3f9fb06da09bb788c55e5b
Asgar-Asgarov <asgar.y.asgarov@gmail.com>
2023-05-24 14:42:35 +0400

    feature:sx property added for responsivness of box component

--- frontend/src/components/HeroBanner.js
+++ frontend/src/components/HeroBanner.js
@@ -3,7 +3,12 @@ import { Box, Stack, Typography } from "@mui/material";
 
 const HeroBanner = () => {
   return (
-    <Box>
+    <Box 
+    sx={{
+      mt:{lg:'212px',xs:'70px'},
+      ml:{sm:'50px'}
+    }}
+    >
       <Typography color="#FF2625" fontWeight="600" fontSize="26px">
         Fitness Club
       </Typography>
