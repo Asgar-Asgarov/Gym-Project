853e2bad80f4d7064167d225b1a041d03ccef633
Asgar-Asgarov <asgar.y.asgarov@gmail.com>
2023-05-26 19:11:16 +0400

    feature:Search part added,Responsivness provided with sx width property

--- frontend/src/components/SearchExercises.js
+++ frontend/src/components/SearchExercises.js
@@ -10,10 +10,24 @@ const SearchExercises = () => {
           sx={{
             fontSize:{lg:'44px',xs:'30px'}
           }}
+          mb="50px" textAlign="center"
       >
         Awesome Exercises You <br/>
         Should Know
       </Typography>
+      <Box position="relative" mb="72px" >
+      <TextField
+      sx={{input:{fontWeight:'700',
+     border:'none',borderRadius:'4px'},
+      width:{lg:'1170px',xs:'350px'}
+  }}
+      height="76px"
+      value=""
+      onChange={(e)=>{}}
+      placeholder="Search Exercises"
+      type="text"
+      />
+      </Box>
 
     </Stack>
   )
