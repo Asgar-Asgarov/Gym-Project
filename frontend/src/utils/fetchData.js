ebc0782ad89417ee4e3652fea5d4b42fb564b003
Asgar-Asgarov <asgar.y.asgarov@gmail.com>
2023-05-29 15:44:22 +0400

    feature:APi Key hidden

--- frontend/src/utils/fetchData.js
+++ frontend/src/utils/fetchData.js
@@ -1,3 +1,10 @@
+export const exerciseOptions = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+      'X-RapidAPI-Key': 
+    }
+  };
 export const fetchData = async (url,options)=>
 {
     const response =await fetch(url,options);
