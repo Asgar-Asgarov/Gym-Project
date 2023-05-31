30e1a633d782a77910e8bcfc58535d4fca98d80a
Asgar-Asgarov <asgar.y.asgarov@gmail.com>
2023-05-31 22:04:03 +0400

    feature:BodyPart sx property written with  conditional operator

--- frontend/src/components/BodyPart.js
+++ frontend/src/components/BodyPart.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React from 'react';
+import {Stack,Typography} from '@mui/material';
 
-const BodyPart = () => {
+import Icon from '../assets/icons/gym.png'
+
+const BodyPart = ({item,setBodyPart,bodyPart}) => {
   return (
-    <div>BodyPart</div>
+    <Stack
+    type="button"
+    alignItems="center"
+    justifyContent="center"
+    className="bodyPart-card"
+    sx={
+       bodyPart === item ? {
+        borderTop:'4px solid #ff2625',
+        backgroundColor:'#fff'
+       } : ''
+    }
+    >
+        <img src={Icon} alt="dumbbell" styles={{width:'40px',height:'40px'}}/>
+    </Stack>
   )
 }
 
