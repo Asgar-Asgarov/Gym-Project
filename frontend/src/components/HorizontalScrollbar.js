3fe1917168f35421c20f2ec026fc07166aebc663
Asgar-Asgarov <asgar.y.asgarov@gmail.com>
2023-05-31 19:03:58 +0400

    feature:bodyPart and exercises states moved to home page for using entire app

--- frontend/src/components/HorizontalScrollbar.js
+++ frontend/src/components/HorizontalScrollbar.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React from 'react';
+import {Box} from '@mui/material';
 
-const HorizontalScrollbar = () => {
+import BodyPart  from './BodyPart';
+
+const HorizontalScrollbar = ({data,bodyPart,setBodyPart}) => {
   return (
-    <div>HorizontalScrollbar</div>
+    <div>
+        {data.map((item)=>(
+           <Box
+           key={item.id||item}
+           itemId={item.id||item}
+           title={item.id||item}
+           m="0 40px"
+           >
+          <BodyPart item={item} bodyPart={bodyPart} setBodyPart={setBodyPart}/>
+           </Box> 
+        )
+        )}
+    </div>
   )
 }
 
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,17 +1,27 @@
-import React,{useState} from 'react';
-import { Box } from '@mui/material';
-import HeroBanner from '../components/HeroBanner';
-import Exercises from '../components/Exercises';
-import SearchExercises from '../components/SearchExercises';
+import React, { useState } from "react";
+import { Box } from "@mui/material";
+import HeroBanner from "../components/HeroBanner";
+import Exercises from "../components/Exercises";
+import SearchExercises from "../components/SearchExercises";
 
 const Home = () => {
+  const [bodyPart, setBodyPart] = useState("all");
+  const [exercises, setExercises] = useState([]);
   return (
-   <Box>
-    <HeroBanner/>
-    <SearchExercises/>
-    <Exercises/>
-   </Box>
-  )
-}
+    <Box>
+      <HeroBanner />
+      <SearchExercises
+        setExercises={setExercises}
+        bodyPart={bodyPart}
+        setBodyPart={setBodyPart}
+      />
+      <Exercises
+        setExercises={setExercises}
+        bodyPart={bodyPart}
+        setBodyPart={setBodyPart}
+      />
+    </Box>
+  );
+};
 
-export default Home
+export default Home;
