fd4809dd7ab98d4d9c250829ce6d858806e36ddc
Asgar-Asgarov <asgar.y.asgarov@gmail.com>
2023-06-01 21:32:25 +0400

    feature:arrow added HorizontalScrollbar

--- frontend/src/components/BodyPart.js
+++ frontend/src/components/BodyPart.js
@@ -1,25 +1,34 @@
-import React from 'react';
-import {Stack,Typography} from '@mui/material';
+import React from "react";
+import { Stack, Typography } from "@mui/material";
 
-import Icon from '../assets/icons/gym.png'
+import Icon from "../assets/icons/gym.png";
 
-const BodyPart = ({item,setBodyPart,bodyPart}) => {
+const BodyPart = ({ item, setBodyPart, bodyPart }) => {
   return (
     <Stack
-    type="button"
-    alignItems="center"
-    justifyContent="center"
-    className="bodyPart-card"
-    sx={
-       bodyPart === item ? {
-        borderTop:'4px solid #ff2625',
-        backgroundColor:'#fff'
-       } : ''
-    }
+      type="button"
+      alignItems="center"
+      justifyContent="center"
+      className="bodyPart-card"
+      sx={{
+        borderTop: bodyPart === item ? "4px solid #ff2625" : "",
+        backgroundColor: "#fff",
+        width: "270px",
+        height: "280px",
+        cursor: "pointer",
+        gap: "47px"
+      }}
     >
-        <img src={Icon} alt="dumbbell" styles={{width:'40px',height:'40px'}}/>
+      <img
+        src={Icon}
+        alt="dumbbell"
+        styles={{ width: "40px", height: "40px" }}
+      />
+      <Typography>
+        {item}
+      </Typography>
     </Stack>
-  )
-}
+  );
+};
 
-export default BodyPart
+export default BodyPart;
