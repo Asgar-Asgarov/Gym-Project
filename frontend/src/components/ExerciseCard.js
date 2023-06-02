fb5bcdd9e4605f5dc7d698aa860ddceaae5a9808
Asgar-Asgarov <asgar.y.asgarov@gmail.com>
2023-06-02 12:35:16 +0400

    feature:Buttons added to ExerciseCard

--- frontend/src/components/ExerciseCard.js
+++ frontend/src/components/ExerciseCard.js
@@ -8,6 +8,27 @@ const ExerciseCard = ({exercise}) => {
         <img src={exercise.gifUrl} alt={exercise.name} 
         Loading="lazy"
         />
+        <Stack direction="row">
+            <Button sx={{ml:'21px',color:'#fff',background:'#ffa9a9',
+            fontSize:'14px',
+            borderRadius:'20px',
+            textTransform:'capitalize'
+           }}>
+           {exercise.bodyPart}
+            </Button>
+            <Button sx={{ml:'21px',color:'#fff',background:'#fcc757',
+            fontSize:'14px',
+            borderRadius:'20px',
+            textTransform:'capitalize'
+           }}>
+           {exercise.target}
+            </Button>
+        </Stack>
+        <Typography ml="21px" color="#000" fontWeight="bold"
+        mt="11px" pb="10px" textTransform="capitalize"
+        >
+        {exercise.name}
+        </Typography>
     </Link>
   )
 }
