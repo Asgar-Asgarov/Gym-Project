9a9a3f9eec2def83b8986634bddc50bb8a9d81c5
Asgar-Asgarov <asgar.y.asgarov@gmail.com>
2023-06-02 11:13:24 +0400

    feature:ExerciCard added

--- frontend/src/components/Exercises.js
+++ frontend/src/components/Exercises.js
@@ -3,6 +3,7 @@ import Pagination from "@mui/material/Pagination";
 import { Box, Stack, Typography } from "@mui/material";
 
 import { exerciseOptions, fetchData } from "../utils/fetchData";
+import ExerciseCard from "./ExerciseCard";
 
 const Exercises = ({ exercises, setExercises, bodyPart }) => {
   return (
@@ -17,7 +18,7 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
         justifyContent="center"
       >
         {exercises.map((exercise, index) => (
-          <p>{exercise.name}</p>
+          <ExerciseCard key={index} exercise={exercise}/>
         ))}
       </Stack>
     </Box>
