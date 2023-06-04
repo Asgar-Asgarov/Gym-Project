80808d317b6865957905211e4b89450830a25770
Asgar-Asgarov <asgar.y.asgarov@gmail.com>
2023-06-04 11:21:10 +0400

    feature:exerciseDetail destructed

--- frontend/src/components/Detail.js
+++ frontend/src/components/Detail.js
@@ -1,6 +1,12 @@
-import React from 'react'
+import React from 'react';
+import { Typography, Stack, Button } from '@mui/material';
 
-const Detail = () => {
+import BodyPartImage from '../assets/icons/body-part.png';
+import TargetImage from '../assets/icons/target.png';
+import EquipmentImage from  '../assets/icons/equipment.png'
+
+const Detail = ({exerciseDetail}) => {
+  const{bodyPart,gifUrl,name,target,equipment} = exerciseDetail;
   return (
     <div>Detail</div>
   )
