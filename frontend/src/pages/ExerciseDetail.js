3c8afbab8e1ef4b4e3771655df68c6b039ba4708
Asgar-Asgarov <asgar.y.asgarov@gmail.com>
2023-06-04 09:42:38 +0400

    feature:Detail,ExerciceseVideos,SimilarExercises component created

--- frontend/src/pages/ExerciseDetail.js
+++ frontend/src/pages/ExerciseDetail.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react';
+import { useParams } from 'react-router-dom';
+import {Box}  from '@mui/material';
+
+import Detail from '../components/Detail';
+import ExerciseVideos from '../components/ExerciseVideos';
+import SimilarExercises from '../components/SimilarExercises';
+
+import {exerciseOptions,fetchData} from '../utils/fetchData';
 
 const ExerciseDetail = () => {
   return (
-    <div>ExerciseDetail</div>
+  <Box>
+    <Detail/>
+    <ExerciseVideos/>
+    <SimilarExercises/>
+  </Box>
   )
 }
 
