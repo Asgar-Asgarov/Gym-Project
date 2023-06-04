33b4a298fbea47f3714c0941f6ea540dfb50096b
Asgar-Asgarov <asgar.y.asgarov@gmail.com>
2023-06-04 10:48:52 +0400

    feature:UseEffect works when id changes

--- frontend/src/pages/ExerciseDetail.js
+++ frontend/src/pages/ExerciseDetail.js
@@ -16,6 +16,9 @@ const ExerciseDetail = () => {
    const fetchExerciseData = async () => {
    const exerciseDbUrl = 'https://exercisedb.p.rapidapi.com';
    const youtubeSearchUrl = 'https://youtube-search-and-download.p.rapidapi.com';
+
+   const exerciseDetailData = await fetchData(`${exerciseDbUrl}/exercises/${id}`,exerciseOptions);
+   setExerciseDetail(exerciseDetailData);
    }
    fetchExerciseData();
  }, [id])
