1b0304a89343a63b2d6652ae4eed7723b6c321cd
Asgar-Asgarov <asgar.y.asgarov@gmail.com>
2023-06-04 15:18:26 +0400

    feature:ExerciseDetail icons part added

--- frontend/src/utils/fetchData.js
+++ frontend/src/utils/fetchData.js
@@ -5,6 +5,15 @@ export const exerciseOptions = {
       'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY
     }
   };
+
+  const youtubeOptions = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY ,
+      'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
+    }
+  };
+
 export const fetchData = async (url,options)=>
 {
     const response =await fetch(url,options);
