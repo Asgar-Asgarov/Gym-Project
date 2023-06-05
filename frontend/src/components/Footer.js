0918eea67a72e90cc94ac8ba3c32b9d23527a6ca
Asgar-Asgarov <asgar.y.asgarov@gmail.com>
2023-06-05 22:24:46 +0400

    feature:Footer added

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -5,8 +5,13 @@ import Logo from '../assets/images/Logo-1.png'
 
 const Footer = () => {
   return (
-    <Box>
-      
+    <Box mt="80px" bgcolor="#fff3f4">
+   <Stack gap="40px" alignItems="center" px="40px" pt="24px">
+    <img src={Logo} alt="logo" width="200px" height="40px"/>
+    <Typography variant="h5" pb="40px" mt="40px">
+      Made By Asgar
+    </Typography>
+   </Stack>
     </Box>
   )
 }
